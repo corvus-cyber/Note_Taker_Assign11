62baaf54fef06d759e6812695c6ddd7456b4d715
Corvus <wyatta.hancock@gmail.com>
2020-08-03 10:24:55 -0600

    added beginnings of boiler plate

---
+++ public/js/server.js
@@ -0,0 +1,25 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+
+var app = express();
+var PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+// =============================================================
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+
+app.listen(PORT, function() {
+console.log("App listening on PORT " + PORT);
+});
